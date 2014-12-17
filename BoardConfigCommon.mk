3ec9c98f9f7eda5c5a1e782e848977c18c25073b
777jon <jcruzdeveloper@gmail.com>
2014-12-17 18:53:40 -0500

    g3: make zip flash faster
    Allows the block ota zips to be built and as a result zip flash faster.
    
    Dependent on:
    - http://gerrit.aospal.com/#/c/492/
    - http://gerrit.aospal.com/#/c/494/

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,6 +47,9 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000
 TARGET_KERNEL_SOURCE := kernel/lge/g3
 
+# OTA
+TARGET_USES_BLOCK_BASED_OTA := false
+
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 
